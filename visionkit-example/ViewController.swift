e9404d07a482cd33c02d55f1a466bcad2cd82bb4
Yusuke Binsaki <yusuke.binsaki@itage.co.jp>
2019-10-31 10:02:50 +0900

    Initial Commit

---
+++ visionkit-example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  visionkit-example
+//
+//  Created by 14-0254 on 2019/10/31.
+//  Copyright © 2019 Yusuke Binsaki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
